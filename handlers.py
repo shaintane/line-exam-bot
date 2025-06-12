cb5ec5b3cb4e4559ff794645c5338d90c54d546a
shaintane <shaintane@gmail.com>
2025-06-12 15:29:15 +0800

    handlers.py

--- handlers.py
+++ handlers.py
@@ -5,9 +5,5 @@ def process_message(event, line_bot_api, client, user_sessions, registration_buf
     user_id = event.source.user_id
     user_input = event.message.text.strip()
 
-    # 管理者邏輯
-    if handle_admin_commands(user_input, user_id, line_bot_api, event):
-        return
-
     # 測驗流程邏輯
     handle_exam_logic(user_input, user_id, event, line_bot_api, client, user_sessions, registration_buffer)
